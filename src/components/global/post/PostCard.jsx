bee0a8b0e6fb7a32af6ce0042fda618e80e552d4
Lateef-at-africa <Lateef.yunus@styleit.africa>
2025-03-17 13:28:42 -0700

    added report and delete button to the post card

--- src/components/global/post/PostCard.jsx
+++ src/components/global/post/PostCard.jsx
@@ -14,10 +14,17 @@ import { Button } from '@/components/ui/button'
 
 const PostCard=({post,follow}) => {
     const [isCommentOpened,setIsCommentOpened]=useState(false)
+    const [isReportOpened,setIsReportOpened]=useState(false)
   return (
     <div className='max-w-[480px] mx-auto mt-10'>
                  
-    <div className='border border-gray-200 rounded-2xl  text-sm  p-3.5'>
+    <div className='relative border border-gray-200 rounded-2xl  text-sm  p-3.5'>
+        {
+            isReportOpened&&<div className='absolute right-6 top-7 rounded-3xl bg-transparent  backdrop-filter backdrop-blur-md text-white pl-5 py-5 w-[200px] '>
+            <p className='capitalize text-lg cursor-pointer text-red-500 font-[500]'>delete</p>
+            <p className='capitalize text-lg cursor-pointer text-yellow-500 font-[500]'>report</p>
+        </div>
+        }
         <div className='flex justify-between items-center '>
         <User
         userProps={{
@@ -27,28 +34,22 @@ const PostCard=({post,follow}) => {
                 container:' flex items-center gap-3 font-[700] text-lg font-lato'
             }}/>
 
-            <div>
-                
-            </div>
+           
             {
-                follow ? 
-            <div className='flex gap-6 items-center '>
-                <Button className="text-primary p-0 bg-white mt-3d hover:bg-white shadow-none block text-lg">Follow</Button>
 
-            <div className='relative mt-6 cursor-pointer'>
-                <Indicator className='h-1 w-1 absolute bottom-0 right-0 rounded-full bg-black'/>
+            <div className='flex gap-3s items-center '  >
+                {
+                follow&&
+                <Button className="text-primary -mt-1.5  p-0 bg-white hover:bg-white shadow-none block text-lg">Follow</Button>
+                }
+
+                <div className='relative mt-s6 w-[25px] self-start h-[29px] cursor-pointer' onClick={()=>setIsReportOpened(!isReportOpened)}>
                 <Indicator className='h-1 w-1 absolute bottom-2 right-0 rounded-full bg-black'/>
                 <Indicator className='h-1 w-1 absolute bottom-4 right-0 rounded-full bg-black'/>
+                <Indicator className='h-1 w-1 absolute bottom-6 right-0 rounded-full bg-black'/>
 
+                </div>
             </div>
-            </div>:
-             <div className='relative mt-6'>
-             <Indicator className='h-1 w-1 absolute bottom-0 right-0 rounded-full bg-black'/>
-             <Indicator className='h-1 w-1 absolute bottom-2 right-0 rounded-full bg-black'/>
-             <Indicator className='h-1 w-1 absolute bottom-4 right-0 rounded-full bg-black'/>
-
-         </div>
-
             }
 
         </div>
