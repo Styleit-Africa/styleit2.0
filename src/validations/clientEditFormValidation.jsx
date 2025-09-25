0daa13ec1a7b01ba396ee10a51072a317b6ecc44
lateef-at-africa <Lateef.yunus@styleit.africa>
2025-09-25 14:27:30 +0100

    removed bank name and account from client profile and added validation to both client and designer edit form profile

--- src/validations/clientEditFormValidation.jsx
+++ src/validations/clientEditFormValidation.jsx
@@ -1,6 +1,6 @@
 import { z } from "zod"
  
- export const editFormSchema = z.object({
+ export const clientEditFormSchema = z.object({
    email: z.string().email().min(2,"Email must be at least 2 characters."),
    firstName: z.string().min(2, "First name must be at least 2 characters."),
    lastName: z.string().min(2,"Last name must be at least 2 characters."),
